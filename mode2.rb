edc1a1d01d06ee74ea1828cdc30a3406ea4d3c8e
Muhammad Haseeb <haseeb.nazeer@devsinc.com>
2022-08-02 12:48:11 +0500

    removing rubocop error Assignment Branch Condition

--- mode2.rb
+++ mode2.rb
@@ -6,43 +6,59 @@ module MonthHighestTemperatue
     def self.month_highest_temp_day(file_name)
       sum = 0
       File.open(file_name, 'r') do |opened_file|
-        opened_file.readline
-        max_temp_index = opened_file.readline.split(',').find_index('Max TemperatureC')
-        lines = opened_file.readlines
-        lines.each do |line|
-          sum += line.split(',')[max_temp_index].to_i
-        end
+        files_data = opened_file.readlines
+        files_data.delete("\r\n")
+        max_temp_index = files_data[0].split(',').find_index('Max TemperatureC')
+        lines = files_data[1, files_data.length - 1]
+        sum = month_highest_temp_day_helper(lines, max_temp_index)
         sum /= lines.length
       end
       "Average Highest : #{sum}C"
+    end
+
+    def self.month_highest_temp_day_helper(lines, max_temp_index)
+      lines.inject(0) do |sum, line|
+        sum + line.split(',')[max_temp_index].to_i
+      end
     end
 
     def self.month_lowest_temp_day(file_name)
       sum = 0
       File.open(file_name, 'r') do |opened_file|
-        opened_file.readline
-        max_temp_index = opened_file.readline.split(',').find_index('Min TemperatureC')
-        lines = opened_file.readlines
-        lines.each do |line|
-          sum += line.split(',')[max_temp_index].to_i
-        end
+        files_data = opened_file.readlines
+        files_data.delete("\r\n")
+        max_temp_index = files_data[0].split(',').find_index('Max TemperatureC')
+        lines = files_data[1, files_data.length - 1]
+        sum = month_lowest_temp_day_helper(lines, max_temp_index)
         sum /= lines.length
       end
-      "Average Highest : #{sum}C"
+      "Average Lowest : #{sum}C"
+    end
+
+    def self.month_lowest_temp_day_helper(lines, max_temp_index)
+      lines.inject(0) do |sum, line|
+        sum + line.split(',')[max_temp_index].to_i
+      end
     end
 
     def self.month_most_humid_day(file_name)
       sum = 0
       File.open(file_name, 'r') do |opened_file|
-        opened_file.readline
-        max_temp_index = opened_file.readline.split(',').find_index('Max Humidity')
-        lines = opened_file.readlines
-        lines.each do |line|
-          sum += line.split(',')[max_temp_index].to_i
-        end
+        files_data = opened_file.readlines
+        files_data.delete("\r\n")
+        max_temp_index = files_data[0].split(',').find_index('Max Humidity')
+        lines = files_data[1, files_data.length - 1]
+        sum = month_lowest_temp_day_helper(lines, max_temp_index)
+
         sum /= lines.length
       end
-      "Average Highest : #{sum}%"
+      "Average Humidity : #{sum}%"
+    end
+
+    def self.month_most_humid_day_helper(lines, max_temp_index)
+      lines.inject(0) do |sum, line|
+        sum + line.split(',')[max_temp_index].to_i
+      end
     end
   end
 end
--- weatherman.rb
+++ weatherman.rb
@@ -1,24 +1,29 @@
 require_relative 'mode1'
 require_relative 'mode2'
 require_relative 'mode3'
-if ARGV.size.zero?
+
+if ARGV.size.zero? || ARGV.size < 3
   puts 'Please pass arguments to the files'.red
 else
   mode, year, file_path = ARGV
-  case mode
-  when '-e'
-    puts YearHighestTemperatue::YearHighestTemperatueClass.year_highest_temp_day(file_path, year)
-    puts YearHighestTemperatue::YearHighestTemperatueClass.year_lowest_temp_day(file_path, year)
-    puts YearHighestTemperatue::YearHighestTemperatueClass.year_highest_temp_day(file_path, year)
-  when '-a'
-    month_name = Date.parse("#{year.split('/')[0]}-#{year.split('/')[1]}-01").strftime('%b')
-    file_path = "#{file_path}/#{file_path}_#{year.split('/')[0]}_#{month_name}.txt"
-    puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_highest_temp_day(file_path)
-    puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_lowest_temp_day(file_path)
-    puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_most_humid_day(file_path)
-  when '-c'
-    month_name = Date.parse("#{year.split('/')[0]}-#{year.split('/')[1]}-01").strftime('%b')
-    file_path = "#{file_path}/#{file_path}_#{year.split('/')[0]}_#{month_name}.txt"
-    MonthHighestTemperatueChart::MonthHighestTemperatueChartClass.month_analysis_chart(file_path)
+  if File.directory? file_path
+    case mode
+    when '-e'
+      puts YearHighestTemperatue::YearHighestTemperatueClass.year_highest_temp_day(file_path, year)
+      puts YearHighestTemperatue::YearHighestTemperatueClass.year_lowest_temp_day(file_path, year)
+      puts YearHighestTemperatue::YearHighestTemperatueClass.year_most_humid_day(file_path, year)
+    when '-a'
+      month_name = Date.parse("#{year.split('/')[0]}-#{year.split('/')[1]}-01").strftime('%b')
+      file_path = "#{file_path}/#{file_path}_#{year.split('/')[0]}_#{month_name}.txt"
+      puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_highest_temp_day(file_path)
+      puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_lowest_temp_day(file_path)
+      puts MonthHighestTemperatue::MonthHighestTemperatueClass.month_most_humid_day(file_path)
+    when '-c'
+      month_name = Date.parse("#{year.split('/')[0]}-#{year.split('/')[1]}-01").strftime('%b')
+      file_path = "#{file_path}/#{file_path}_#{year.split('/')[0]}_#{month_name}.txt"
+      MonthHighestTemperatueChart::MonthHighestTemperatueChartClass.month_analysis_chart(file_path)
+    end
+  else
+    puts 'Please provide a right directory path'.red
   end
 end
