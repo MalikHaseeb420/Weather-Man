ba929adeda5c9bd5c0619bb4f78d145cd15daf6c
Muhammad Haseeb <haseeb.nazeer@devsinc.com>
2022-08-03 18:39:38 +0500

    removing extra conditions for refactoriztion

--- mode3.rb
+++ mode3.rb
@@ -12,12 +12,10 @@ module MonthHighestTemperatueChart
 
     def self.month_analysis_chart_helper(lines, max_temp_index, min_temp_index)
       lines.each_with_index do |line, i|
-        next if line.split(',')[max_temp_index].nil?
+        next if line.split(',')[max_temp_index].nil? || line.split(',')[min_temp_index].nil?
 
         max_temp = line.split(',')[max_temp_index].to_i
         print_highest_chart(max_temp, i)
-        next if  line.split(',')[min_temp_index].nil?
-
         min_temp = line.split(',')[min_temp_index].to_i
         print_lowest_chart(min_temp, i)
       end
